5bf31da7820ffa1d321cf955648a11da230c77ae
SebastianWiesenhuetter <swiesenhuetter@gmail.com>
2024-07-24 13:28:01 +0200

    reworked landing page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Landingpage7 v-if="!activateMainpage" @leave="leaveLandingpage"/>
+    <Landingpage25 v-if="!activateMainpage" @leave="leaveLandingpage"/>
     <Mainpage v-if="activateMainpage" :activeImage="activeImage" @setActiveImage="setActiveImage"/>
   </div>
 </template>
@@ -8,14 +8,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 // import ThreejsScene from './components/ThreejsScene.vue'
-import Mainpage from './components/Mainpage.vue'
-import Landingpage7 from './components/Landingpage7.vue'
+import Mainpage from './components/Mainpage4.vue'
+import Landingpage25 from './components/Landingpage25.vue'
 
 const activeImage = ref(0)
 const setActiveImage = (index: number) => {
   activeImage.value = index
 }
-const activateMainpage = ref(false)
+const activateMainpage = ref(true)
 const leaveLandingpage = () => {
   activateMainpage.value = true
 }
